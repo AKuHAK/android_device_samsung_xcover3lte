38b3dd166b433220e0c4b5fd0cb82c345951b017
AKuHAK <akuhak@gmail.com>
2017-03-17 11:43:28 +0200

    Fix fix fix

--- tools/Android.mk
+++ tools/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := mkbootimg.c
 LOCAL_STATIC_LIBRARIES := libmincrypt
 
-LOCAL_MODULE := mkbootimg
+LOCAL_MODULE := pxa1088-mkbootimg
 
 include $(BUILD_HOST_EXECUTABLE)
 
